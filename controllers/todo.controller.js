7e301b0c656baf3926720fed0ed9bdd0286154f5
Ana Junquera Vara <anajunqueravs@gmail.com>
2020-07-05 10:47:53 +0200

    Install node-mocks-http / Test 3 - test 4

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,4 +1,6 @@
 const TodoModel = require("../model/todo.model");
-exports.createTodo = () => {
-  TodoModel.create();
+exports.createTodo = (req, res, next) => {
+  const createModel = TodoModel.create(req.body);
+  // test 3 res.status(201).send();
+  res.status(201).json(createModel);
 };
--- tests/units/todocontroller.test.js
+++ tests/units/todocontroller.test.js
@@ -1,9 +1,22 @@
 const TodoController = require("../../controllers/todo.controller");
 const TodoModel = require("../../model/todo.model");
+const httpMocks = require("node-mocks-http");
+const newTodo = require("../mock-data/new-todo.json");
 
 TodoModel.create = jest.fn();
+// Before run each test: create a mock object with all the same methods that the original implementation
+let req, res, next;
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
 
 describe("TodoController.createTodo", () => {
+  //Insted of repeat this code in each it
+  beforeEach(() => {
+    req.body = newTodo;
+  });
   // Test 1
   it("should have a create function", () => {
     expect(typeof TodoController.createTodo).toBe("function");
@@ -11,7 +24,22 @@ describe("TodoController.createTodo", () => {
 
   // Test 2
   it("should call TodoModel.create", () => {
-    TodoController.createTodo();
-    expect(TodoModel.create).toBeCalled();
+    TodoController.createTodo(req, res, next);
+    // expect(TodoModel.create).toBeCalled();
+    expect(TodoModel.create).toBeCalledWith(newTodo);
+  });
+
+  // Test 3
+  it("should return 201 respond code", () => {
+    TodoController.createTodo(req, res, next);
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy;
+  });
+
+  // Test 4
+  it("should return json body in response", () => {
+    TodoModel.create.mockReturnValue(newTodo);
+    TodoController.createTodo(req, res, next);
+    expect(res._getJSONData()).toStrictEqual(newTodo);
   });
 });
